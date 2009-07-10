cf954307bbe87663077724e0faa628f0b8425a04
rawlins <user@email>
2009-07-10 19:01:30 +0000

    Updated test_suite.bash
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio/Testing@224 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- test_suite.bash
+++ test_suite.bash
@@ -11,19 +11,19 @@
 #	implementation, edit this script accordingly.
 
 host=${HOSTNAME%%.*}
-root=~
+root=~/mrl # This line needs to be edited to be the directory where FEBio resides
 if [ $HOSTTYPE == "i386" ]; then
 	plat=osx
 elif [ $HOSTTYPE == "ia64" ]; then
 	plat=alt
-elif [ $HOSTTYPE == "i686" ]; then
-	root=/cygdrive/s
+elif [ $HOSTTYPE == "i686" ]; then # Cygwin
+	root=/cygdrive/s/mrl # This line needs to be edited to be the directory where FEBio resides
 	host=win
 	plat=exe
 elif [ $HOSTTYPE == "x86_64" ]; then
 	plat=lnx
 fi
-febio=${root}/mrl/FEBio/bin/febio.$plat
+febio=${root}/FEBio/bin/febio.$plat
 
 echo 'File,Equations,Specified Time Steps,Solve Time,Elapsed Time,Steps Completed,Termination,Equil Iter,Stiff Reform' > ${host}'_results.csv'
 
@@ -31,40 +31,36 @@ if [ ! -d platform/$host ]; then
 	mkdir platform/$host
 fi
 
-for dir in $(ls -1p Input/ | grep /); do
-	echo 'dir = ' $dir
-	cd Input/$dir
-	for infile in $(ls *.feb); do
-		if [ $host == 'win' ]; then
-			awk 'sub("$", "\r")' $infile > ${infile%%.*}_win.feb
-			input=${infile%%.*}_win.feb
-		else
-			input=$infile
-		fi
-		echo $input
-		$febio -i $input > /dev/null
-		log=${input%%.*}.log
-		plt=${input%%.*}.plt
-		eqns=$(awk '/Nr of equations/ {print $6; exit}' $log)
-		tmsteps=$(awk '/Number of timesteps/ {print $6}' $log)
-		solve_tm=$(awk '/Time in solver/ {print $4}' $log)
-		elapse_tm=$(awk '/Elapsed time/ {print $4}' $log)
-		steps=$(awk '/steps completed/ {print $8}' $log)
-		eq_it=$(awk '/Total number of equilibrium/ {print $8}' $log)
-		st_re=$(awk '/Total number of stiffness/ {print $8}' $log)
-		if [ -n "$(grep 'E R R O R' $log)" ]; then
-			term=Error
-		elif [ -n "$(grep 'N O R M A L' $log)" ]; then
-			term=Normal
-		else
-			term=''
-		fi
-		echo $input,$eqns,$tmsteps,$solve_tm,$elapse_tm,$steps,$term,$eq_it,$st_re >> ../../${host}'_results.csv'
-		mv $log ../../platform/$host
-		rm $plt
-		if [ $host == 'win' ]; then
-			rm $input
-		fi
-	done
-	cd ../..
+cd Input
+for infile in $(ls *.feb); do
+	if [ $host == 'win' ]; then
+		awk 'sub("$", "\r")' $infile > ${infile%%.*}_win.feb
+		input=${infile%%.*}_win.feb
+	else
+		input=$infile
+	fi
+	echo $input
+	$febio -i $input > /dev/null
+	log=${input%%.*}.log
+	plt=${input%%.*}.plt
+	eqns=$(awk '/Nr of equations/ {print $6; exit}' $log)
+	tmsteps=$(awk '/Number of timesteps/ {print $6}' $log)
+	solve_tm=$(awk '/Time in solver/ {print $4}' $log)
+	elapse_tm=$(awk '/Elapsed time/ {print $4}' $log)
+	steps=$(awk '/steps completed/ {print $8}' $log)
+	eq_it=$(awk '/Total number of equilibrium/ {print $8}' $log)
+	st_re=$(awk '/Total number of stiffness/ {print $8}' $log)
+	if [ -n "$(grep 'E R R O R' $log)" ]; then
+		term=Error
+	elif [ -n "$(grep 'N O R M A L' $log)" ]; then
+		term=Normal
+	else
+		term=''
+	fi
+	echo $input,$eqns,$tmsteps,$solve_tm,$elapse_tm,$steps,$term,$eq_it,$st_re >> ../${host}'_results.csv'
+	mv $log ../platform/$host
+	rm $plt
+	if [ $host == 'win' ]; then
+		rm $input
+	fi
 done
