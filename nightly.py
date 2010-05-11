bf7aabea870bd017935e1a380d6a4ce100d5b9fa
rawlins <user@email>
2010-05-11 17:01:06 +0000

    Edited nightly.py to run on Win7.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio/Testing@1017 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.py
+++ nightly.py
@@ -21,16 +21,16 @@ solvers = ['pardiso', 'skyline', 'superlu']
 
 # Set the platform and specific solvers
 if opsys == 'i386':
-	plat = 'osx'
+        plat = 'osx'
 elif opsys == 'ia64':
-	plat = 'alt'
-	solvers = ['pardiso', 'superlu', 'psldlt']
+        plat = 'alt'
+        solvers = ['pardiso', 'superlu', 'psldlt']
 elif opsys == 'x86_64':
-	plat = 'lnx'
+        plat = 'lnx'
 elif sysplat == 'win32':
-	plat = 'win'
+        plat = 'win'
 elif opsys == 'i686':
-	plat = 'lnx32'
+        plat = 'lnx32'
 
 # Use one thread
 os.environ['OMP_NUM_THREADS'] = '1'
@@ -66,11 +66,11 @@ else:
                 subprocess.call(['svn', 'up'])
 
         # Compile FEBio
-	try:
-		shutil.copy(febio, febio.split('.')[0] + '_old.' + plat)
-		shutil.copy(febio_lib, febio_lib.split('.')[0] + '_old.a')
-	except IOError:
-		print "Error copying files"
+        try:
+                shutil.copy(febio, febio.split('.')[0] + '_old.' + plat)
+                shutil.copy(febio_lib, febio_lib.split('.')[0] + '_old.a')
+        except IOError:
+                print "Error copying files"
         command =['make', '-f', 'febio.mk', plat + 'clean' ]
         subprocess.call(command)
         command =['make', '-f', 'febio.mk', plat]
@@ -83,70 +83,70 @@ test.sort()
 #test = ['co01.feb', 'co02.feb']
 
 # keep counters
-norms = 0			# nr of normal terminations
-nerrs = 0			# nr of error terminations
+norms = 0                       # nr of normal terminations
+nerrs = 0                       # nr of error terminations
 
 # exempt problems
 exempt = ['skylineco32','skylineco34'] # These problems require nonsymmetric matrices
 
 #run the test problems
 for solver in solvers:
-	for f in test:
-		# strip the '.feb' from the input file name
-		base = f[:4]
-		if solver + base not in exempt:
-			# define the log and plt files
-			logname = out_dir + solver + '_' + base + '.log'
-			pltname = out_dir + solver + '_' + base + '.plt'
-			# open the dummy file
-			dummy = open(out_dir + "dummy.txt", "w")
-			# run the FEBio problem
-			# we grab the exit value for termination status
-			# TODO: check if the redirection will work on windows
-			#command = febio + ' -i ' + f + ' -o ' + logname + ' -p ' + pltname + \
-			#	' -cnf ~/FEBio/' + solver + '.xml' + ' >& ../dummy.out'
-			command = [febio, '-i', f, '-o', logname, '-p', pltname, \
-				'-cnf', febio_dir + '/' + solver + '.xml']
-			#print command
-			val = subprocess.call(command, stdout=dummy)
+        for f in test:
+                # strip the '.feb' from the input file name
+                base = f[:4]
+                if solver + base not in exempt:
+                        # define the log and plt files
+                        logname = out_dir + solver + '_' + base + '.log'
+                        pltname = out_dir + solver + '_' + base + '.plt'
+                        # open the dummy file
+                        dummy = open(out_dir + "dummy.txt", "w")
+                        # run the FEBio problem
+                        # we grab the exit value for termination status
+                        # TODO: check if the redirection will work on windows
+                        #command = febio + ' -i ' + f + ' -o ' + logname + ' -p ' + pltname + \
+                        #       ' -cnf ~/FEBio/' + solver + '.xml' + ' >& ../dummy.out'
+                        command = [febio, '-i', f, '-o', logname, '-p', pltname, \
+                                '-cnf', febio_dir + '/' + solver + '.xml']
+                        #print command
+                        val = subprocess.call(command, stdout=dummy)
 
-			# create a variable that will store the results of the test
-			# 0: solver
-			# 1: file
-			# 2: Normal/Error termination status
-			# 3: Number of time steps
-			# 4: Number of iterations
-			# 5: Number of RHS evaluations
-			# 6: Number of reformations
-			# 7: Plot file size
-			result = [solver, base, "", 0, 0, 0, 0, 0]
-			
-			# check the return value
-			if val==0:
-				result[2] = 'Normal'
-				norms = norms + 1
-			else:
-				result[2] = 'Error'
-				nerrs = nerrs + 1
-				
-			#search the log file for the convergence info
-			try:
-				flog = open(logname, 'rt')
-				
-				for line in flog:
-					if  line.find("Number of time steps completed"        ) != -1: result[3] = int(line[55:])
-					if  line.find("Total number of equilibrium iterations") != -1: result[4] = int(line[55:])
-					if  line.find("Total number of right hand evaluations") != -1: result[5] = int(line[55:])
-					if  line.find("Total number of stiffness reformations") != -1: result[6] = int(line[55:])
-				flog.close()
-				result[7] = os.path.getsize(pltname)
-			except IOError:
-				result[2] = 'Fail'
-			except OSError:
-				result[2] = 'Fail'
-			print result
-			results.write(str(result) + '\n')
-			dummy.close()
+                        # create a variable that will store the results of the test
+                        # 0: solver
+                        # 1: file
+                        # 2: Normal/Error termination status
+                        # 3: Number of time steps
+                        # 4: Number of iterations
+                        # 5: Number of RHS evaluations
+                        # 6: Number of reformations
+                        # 7: Plot file size
+                        result = [solver, base, "", 0, 0, 0, 0, 0]
+                        
+                        # check the return value
+                        if val==0:
+                                result[2] = 'Normal'
+                                norms = norms + 1
+                        else:
+                                result[2] = 'Error'
+                                nerrs = nerrs + 1
+                                
+                        #search the log file for the convergence info
+                        try:
+                                flog = open(logname, 'rt')
+                                
+                                for line in flog:
+                                        if  line.find("Number of time steps completed"        ) != -1: result[3] = int(line[55:])
+                                        if  line.find("Total number of equilibrium iterations") != -1: result[4] = int(line[55:])
+                                        if  line.find("Total number of right hand evaluations") != -1: result[5] = int(line[55:])
+                                        if  line.find("Total number of stiffness reformations") != -1: result[6] = int(line[55:])
+                                flog.close()
+                                result[7] = os.path.getsize(pltname)
+                        except IOError:
+                                result[2] = 'Fail'
+                        except OSError:
+                                result[2] = 'Fail'
+                        print result
+                        results.write(str(result) + '\n')
+                        dummy.close()
 
 # print a summary to the log file
 results.write("\nSummary:\n")
