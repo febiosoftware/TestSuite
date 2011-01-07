b4dd76ba61edc6c96050a9662d499fb6e1168f03
rawlins <user@email>
2011-01-07 20:51:22 +0000

    Fixed time comparison bug in nightly.py
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio/Testing@1601 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.py
+++ nightly.py
@@ -178,8 +178,8 @@ for solver in solvers:
                                 el_diff = new_el_time - old_el_time
                                 if old_slv_time == 0: slv_denom = 1
                                 if old_el_time == 0: el_denom = 1
-                                if slv_diff <= 2: slv_diff = 0
-                                if el_diff <= 2: el_diff = 0
+                                if abs(slv_diff) <= 2: slv_diff = 0
+                                if abs(el_diff) <= 2: el_diff = 0
                                 # calculate percent change (in increments of 10%) in solve and elapse times
                                 result[9]  = 10*int(10*slv_diff/float(slv_denom))
                                 result[10] = 10*int(10*el_diff/float(el_denom))
