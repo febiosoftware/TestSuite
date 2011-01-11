e71997e40287c55b39b9b5d3bd35af769061e3ca
rawlins <user@email>
2011-01-11 20:36:43 +0000

    Fixed bug in nightly.py
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio/Testing@1615 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.py
+++ nightly.py
@@ -86,9 +86,9 @@ else:
 
 # Define the test problems list.
 os.chdir(febio_dir + "/Testing/Verify")
-#test = glob.glob("*.feb")
-#test.sort()
-test = ['co01.feb', 'co02.feb']
+test = glob.glob("*.feb")
+test.sort()
+#test = ['co01.feb', 'co02.feb']
 
 # keep counters
 norms = 0                       # nr of normal terminations
@@ -176,13 +176,17 @@ for solver in solvers:
                                                 el_min = int(line[18:20])
                                                 el_sec = int(line[21:23])
                                                 old_el_time = el_hr*3600 + el_min*60 + el_sec
+                                slv_denom = (new_slv_time + old_slv_time)/2
+                                el_denom = (new_el_time + old_el_time)/2
+                                if slv_denom == 0: slv_denom = 1
+                                if el_denom == 0: el_denom = 1
                                 slv_diff = new_slv_time - old_slv_time
                                 el_diff = new_el_time - old_el_time
                                 if abs(slv_diff) <= 2: slv_diff = 0
                                 if abs(el_diff) <= 2: el_diff = 0
                                 # calculate percent difference (in increments of 10%) in solve and elapse times
-                                result[9]  = 10*int(20*slv_diff/float(old_slv_time + new_slv_time))
-                                result[10] = 10*int(20*el_diff/float(old_el_time + new_el_time))
+                                result[9]  = 10*int(10*slv_diff/float(slv_denom))
+                                result[10] = 10*int(10*el_diff/float(el_denom))
                                 # get the size of the plotfile
                                 result[7] = os.path.getsize(pltname)
                                 # do a diff on the log file
