fe08b851ddeca5211e1c4e45547b58ca38473259
rawlins <user@email>
2011-02-14 17:20:00 +0000

    Modified nightly_email.py
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio/Testing@1781 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly_email.py
+++ nightly_email.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import platform
-import smtplib
+import os, platform, smtplib
 from email.mime.text import MIMEText
 
 host = platform.node().split('.')[0]
 
 if host == 'winxp': me = 'rawlins@sci.utah.edu'
 else: me = 'scirun-tester@sci.utah.edu'
+os.chdir("C:/FEBio/Testing")
 fp = open("nightly_compare.txt", 'r')
 msg = MIMEText(fp.read())
 fp.close()
