1017d6e884f8238e66883e45f404649dcefc4563
rawlins <user@email>
2011-02-15 21:00:39 +0000

    Added new_model.py
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/FEBio/Testing@1790 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.py
+++ nightly.py
@@ -71,7 +71,7 @@ else:
         out_dir = '/scratch/rawlins/febio_test/'
 
         # Do an svn update on nemo
-        if host == 'swell':
+        if host == 'nemo':
                 subprocess.call(['svn', 'up'])
 
         # Compile FEBio
