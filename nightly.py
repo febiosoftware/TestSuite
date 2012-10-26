3be2c9778924113a77d5c95d6ae9e995f664ae49
rawlins <user@email>
2012-10-26 22:17:47 +0000

    Changed test suite problems to output .xplt
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@3929 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.py
+++ nightly.py
@@ -242,11 +242,13 @@ xplt = ['bp04', 'bp10', 'bp11', 'bp12', 'bp13', 'bp14', 'bp15', 'bp16', 'bp17', 
         'fi13', 'fi14', 'fi15',
         'ma01', 'ma02', 'ma03', 'ma05', 'ma06', 'ma07', 'ma11', 'ma12', 'ma13', 'ma14', 'ma15', 'ma16',
         'mg01', 'mg02',
-        'mi30',
-        'te04',
-        'tr01',
-        'tr02',
-        'tr03']
+        'mi01', 'mi02', 'mi03', 'mi04', 'mi05', 'mi06', 'mi09', 'mi16', 'mi17', 'mi19', 'mi24', 'mi25', 'mi26',
+        'mi27', 'mi28', 'mi29', 'mi30', 'mi31', 'mi32',
+        'ms01', 'ms02', 'ms03', 'ms04', 'mu02', 'mu03',
+        'ri01', 'ri02', 'ri03', 'ri04', 'ri05', 'ri06', 'ri07',
+        'sh01', 'sh02', 'sh03', 'sh04', 'sh05', 'sh07', 'sh10', 'sh13', 'sh14', 'sh15', 'sh16', 
+        'te01', 'te02', 'te03', 'te04', 'te05',
+        'tr01', 'tr02', 'tr03', 'tu01', 'tu02', 'tu03']
 
 #run the test problems
 for solver in solvers:
