70265a3796329a601f10bed1652f9ab5481e6bfd
rawlins <user@email>
2013-01-11 17:38:34 +0000

    Added bp20 to test suite
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4051 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- examples.py
+++ examples.py
@@ -68,7 +68,7 @@ nerrs = 0			# nr of error terminations
 
 # These problems are new, newly modified, or deleted
 new      = []
-modified = ['bs02', 'bs03']
+modified = []
 deleted  = []
 # Open the nightly_std file and a temporary nightly_std file
 b_new = 0
--- update.py
+++ update.py
@@ -1,3 +1,3 @@
-new = []
-modified = ['bs01', 'bs04', 'bs05', 'tr01', 'tr02', 'tr03']
+new = ['bp20']
+modified = []
 deleted = []
