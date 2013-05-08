f00ddeaf25ba36ab2aca7bbf52ca163121165fe9
rawlins <user@email>
2013-05-08 20:16:12 +0000

    Added log data to bp problems
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4404 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.py
+++ nightly.py
@@ -164,7 +164,18 @@ paramopt = ['op01',
 
 
 # These are problems that report extra data fields
-dfield = ['ht01']
+dfield = ['bp04',
+	  'bp11',
+	  'bp12',
+	  'bp13',
+	  'bp14',
+	  'bp15',
+	  'bp16',
+	  'bp17',
+	  'bp18',
+	  'bp19',
+	  'bp20',
+	  'ht01']
 
 # Read the commanline arguments
 if args.find('f') != -1: exempt += slow + inconsistent
