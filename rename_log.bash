21147281abd124dfa3bc561d432a5efba93f6800
rawlins <user@email>
2013-05-22 18:37:20 +0000

    Added rename_log.bash
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4453 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ rename_log.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copies the log file to the standard log file
+
+if [ $# == 0 ]; then
+	echo "Usage: enter the root name of the problem, e.g., co04"
+	exit
+fi
+
+cp pardiso_$1.log pardiso_$1_std.log
