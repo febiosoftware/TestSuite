a57ab3cc55a6a20381891dd8d735f87e63507861
drojas <user@email>
2013-05-23 20:23:46 +0000

    updated update.py
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4457 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['bp05']
+modified = ['bp10', 'bp16', 'bs04', 'bs05','co01', 'co02','co04', 'co07', 'co09', 'co10', 'co13', 'co15', 'co16', 'co18', 'co19', 'co20', 'co21', 'co22']
 deleted = []
