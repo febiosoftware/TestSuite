bb14f1896645f766cfbdc3a7fdacb8ab7ca3503e
rawlins <user@email>
2013-06-15 17:20:55 +0000

    Added dy03, dy04 to mod list
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4542 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,6 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['fi01', 'fi02', 'fi03', 'fi04', 'fi05', 'fi06',
-            'fi07', 'fi08', 'fi09', 'fi10', 'fi11', 'fi12',
-            'fi13', 'fi14', 'fi15', 'fi16']
+modified = ['dy03', 'dy04']
 deleted = []
