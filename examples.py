b206bfae06d94507db98ab86c2ad14992b42ea0f
rawlins <user@email>
2013-07-05 16:49:52 +0000

    Removed problems from mod lists
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4570 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- examples.py
+++ examples.py
@@ -68,7 +68,7 @@ nerrs = 0			# nr of error terminations
 
 # These problems are new, newly modified, or deleted
 new      = []
-modified = ['congneo', 'incneo', 'open_knee']
+modified = []
 deleted  = []
 # Open the nightly_std file and a temporary nightly_std file
 b_new = 0
