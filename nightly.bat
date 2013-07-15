e75744e3740779ed86ca6c4aa7fe6e82aa82d328
rawlins <user@email>
2013-07-15 16:35:39 +0000

    Changed nightly.bat to write compilation and svn output to results file.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4599 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.bat
+++ nightly.bat
@@ -1,6 +1,7 @@
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC"\vcvarsall.bat
-TortoiseProc /command:update /path:"C:\%1\" /closeonend:1
-TortoiseProc /command:update /path:"C:\Testing\" /closeonend:1
+set compare=C:\Testing\%1_Logs\nightly_compare.txt
+call "C:\Program Files\Microsoft Visual Studio 9.0\VC"\vcvarsall.bat > %compare%
+TortoiseProc /command:update /path:"C:\%1\" /closeonend:1 >> %compare%
+TortoiseProc /command:update /path:"C:\Testing\" /closeonend:1 >> %compare%
 if "%1" == "FEBio" vcbuild C:\FEBio\FEBio.sln "Release|Win32"
 if "%1" == "FEBio2" vcbuild C:\FEBio2\VS2008\FEBio2.sln "Release|Win32"
-C:\Testing\nightly.py %* > C:\Testing\%1_Logs\nightly_compare.txt
+C:\Testing\nightly.py %* >> %compare%
