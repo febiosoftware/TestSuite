88c70c4f495f6626d8b3d791e78a75f00b4cdc36
rawlins <user@email>
2013-07-15 17:23:09 +0000

    Combined nightly.bat and nightly_64.bat
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4602 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- nightly.bat
+++ nightly.bat
@@ -1,7 +1,13 @@
 set compare=C:\Testing\%1_Logs\nightly_compare.txt
-call "C:\Program Files\Microsoft Visual Studio 9.0\VC"\vcvarsall.bat
 TortoiseProc /command:update /path:"C:\%1\" /closeonend:1
 TortoiseProc /command:update /path:"C:\Testing\" /closeonend:1
-if "%1" == "FEBio" vcbuild C:\FEBio\FEBio.sln "Release|Win32" > %compare%
-if "%1" == "FEBio2" vcbuild C:\FEBio2\VS2008\FEBio2.sln "Release|Win32" > %compare%
+if %COMPUTERNAME% == "CIBC-RD7" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC"\vcvarsall.bat
+	vcbuild C:\FEBio\FEBio.sln "Release|x64" > %compare%
+	)
+else (
+	call "C:\Program Files\Microsoft Visual Studio 9.0\VC"\vcvarsall.bat
+	if "%1" == "FEBio" vcbuild C:\FEBio\FEBio.sln "Release|Win32" > %compare%
+	if "%1" == "FEBio2" vcbuild C:\FEBio2\VS2008\FEBio2.sln "Release|Win32" > %compare%
+	)
 C:\Testing\nightly.py %* >> %compare%
