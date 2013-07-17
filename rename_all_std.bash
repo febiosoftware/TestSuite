0bdc86875958279f8006870621dbf5186f9745f1
rawlins <user@email>
2013-07-17 22:58:57 +0000

    Changed name of rename bash script.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4611 b505bdfe-9d5c-0410-8600-697d5fc8e487

---
+++ rename_all_std.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Usage: rename all log or rename all xplt to std file.
+
+if [ $# == 0 ]; then
+	echo "Usage: To rename all log or rename all xplt to standard file."
+	echo "rename_all_std.bash log or rename_all_std.bash xplt"
+	exit
+fi
+
+for input in $(ls *.$1); do
+    cp -v $input ./${input%.*}_std.$1
+done
