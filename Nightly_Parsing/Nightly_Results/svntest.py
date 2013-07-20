da05829e529eac5df73bd930a92b3305a3a3ffc3
bhayden <user@email>
2013-07-20 01:43:27 +0000

    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4630 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- Nightly_Parsing/Nightly_Results/svntest.py
+++ Nightly_Parsing/Nightly_Results/svntest.py
@@ -6,8 +6,6 @@ url = '/url:https://gforge.sci.utah.edu/svn/MRLProjects/Testing/Nightly_Parsing/
 current = os.getcwd()
 cwd = current.replace("\\", "/")
 
-path = '/path:' + cwd + '/cibc-rd7.txt'
-print "Path: " + path
-
-subprocess.Popen(['TortoiseProc.exe', '/command:commit', '/message: committing parsing results', url, path, '/closeonend:0'])
-#subprocess.call(['svn', 'ci', parsingFile, '-m', '"Commiting nightly files for Parsing"'])
+path = ' /path:' + cwd + '/cibc-rd7.txt'
+arguments = '/command:commit ' +url + path, '\/message: "committing parsing results" /closeonend:2'
+subprocess.call(['TortoiseProc.exe', arguments])
