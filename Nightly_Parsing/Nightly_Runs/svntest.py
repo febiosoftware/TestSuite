e73b7091c3d4d9eccb213eea16785b24a4ed32de
rawlins <user@email>
2013-09-03 22:48:35 +0000

    Removed svn test from parser
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4809 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- Nightly_Parsing/Nightly_Runs/svntest.py
+++
@@ -1,5 +0,0 @@
-import os, glob, platform, shutil, sys, subprocess, difflib, datetime, time
-
-#SVN Commit the parsing file
-subprocess.call(['svn', 'ci', '-m', '"Commiting nightly files for Parsing"'])
-
