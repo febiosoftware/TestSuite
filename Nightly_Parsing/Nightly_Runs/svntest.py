06a22a23d9d6902891f48e3d7d198fc78adeed59
bhayden <user@email>
2013-09-03 22:16:02 +0000

    "Commiting nightly files for Parsing"
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4807 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- Nightly_Parsing/Nightly_Runs/svntest.py
+++ Nightly_Parsing/Nightly_Runs/svntest.py
@@ -1,5 +1,5 @@
 import os, glob, platform, shutil, sys, subprocess, difflib, datetime, time
 
 #SVN Commit the parsing file
-subprocess.call(['svn', 'commit - m "Updated nightly parser"'])
+subprocess.call(['svn', 'ci', '-m', '"Commiting nightly files for Parsing"'])
 
