dd7b5bfc38dfdd36928dee8751f84a951c9125b6
bhayden <user@email>
2013-09-03 22:42:45 +0000

    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@4808 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- Nightly_Parsing/Parser/Results.py
+++ Nightly_Parsing/Parser/Results.py
@@ -1,3 +1,35 @@
+<<<<<<< .mine
+'''
+Created on Sep 10, 2012
+
+@author: Bryce
+'''
+
+class Results():
+    '''
+    classdocs
+    '''
+    #member variables
+    
+    #This holds the name of the files to parse
+    _files = []
+    
+    #This holds the termination results for each test
+    _current_Errors = {}
+    _current_Output = {}
+    _current_Variations = {}
+    
+    #These hold the values for the standard terminations
+    _standard_Output = {} 
+    _standard_Errors = {}
+    _standard_Missing = {}
+    
+    _variations = {}
+    
+    def setFiles(self,files):
+        filepath ='../Nightly_Runs/'
+        for file in files:
+            self._files.append(filepath+file)    =======
 '''
 Created on Sep 10, 2012
 
@@ -28,4 +60,4 @@ class Results():
     def setFiles(self,files):
         filepath ='../Resources/'
         for file in files:
-            self._files.append(filepath+file)    
+            self._files.append(filepath+file)    >>>>>>> .r4807
