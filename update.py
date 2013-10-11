7e2848ed77f08309a30ab072294f3de3f64ec4f4
rawlins <user@email>
2013-10-11 18:04:35 +0000

    Added problems to mod list for cibc.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@5005 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = []
+modified = ['bp19', 'fi04', 'mi26', 'te01', 'bs04', 'bs05', 'co19', 'co37', 'mi09',
+            'mi19', 'ri02', 'ri06', 'tu01', 'te04', 'bs01', 'fi13', 'ma03', 'mi33']
 deleted = []
