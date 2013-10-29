2061e286db71f71d94a7e4d8901a671b9b179681
rawlins <user@email>
2013-10-29 18:16:40 +0000

    Mod list for cibc-test2
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@5081 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = []
+modified = ['bp05', 'co13', 'co19', 'co41', 'cr01', 'cr02', 'cr03', 'fi01', 'fi14', 'fi16',
+             'ht01', 'ma07', 'mi09', 'fi04', 'dy09', 'co35', 'co16', 'bs01', 'bp19']
 deleted = []
