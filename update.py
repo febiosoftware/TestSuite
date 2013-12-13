adfde110cfa2d9369452366262050165cb350f98
rawlins <user@email>
2013-12-13 19:49:37 +0000

    Testing: Added problems to mod list for cibc-test2
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@5333 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,4 +1,11 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = []
+modified = ['co02','co04','co07','co08','co09','co13','co15','co16','co20','co21',
+            'co22','co25','co26','co27','co30','co32','co39','co41','di01','di03',
+            'dy09','fi01','fi02','fi03','fi04','ma01','ma02','ma03','ma06','ma12',
+            'ma15','ma16','mg01','mi01','mi03','mi04','mi05','mi09','mi16','mi17',
+            'mi19','mi24','mi25','mi27','mp06','mp07','mp08','ms02','ms03','ri01',
+            'ri03','ri04','ri05','sh01','sh02','sh03','sh04','sh05','sh07','sh14',
+            'sh15','sh16','te01','te02','te03','te04','te05','tr02','tr03','tr04',
+            'tu01','tu03']
 deleted = []
