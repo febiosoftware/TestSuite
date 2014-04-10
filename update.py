fda315757f10d565eea60387488a34f7c9f2d5ef
rawlins <user@email>
2014-04-10 21:56:23 +0000

    Testing: changed mod list for Win7 run.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@5775 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['co13','co16','co18','co19','co35','co36','co37','cr01','cr02','dy09','ht01','ma16','mi19','mi31','mi33','mp01','mp08','mu02','mu03','tr01','tr02']
+modified = ['co16','co27','co35','co36','ma07','mi09','mp01','mp08','tr01','tr02']
 deleted = []
