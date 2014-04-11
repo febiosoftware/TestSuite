ef1f0bf78c9c6f246cb08ca84e144acad4c698d2
rawlins <user@email>
2014-04-11 18:22:58 +0000

    Testing: Added logdata to more problems
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@5778 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['bp04','bp15','bp16','bp19','bs01','cr03','dy02']
+modified = ['bp04','bp15','bp16','bp19','bp21','bp22','bs01','bs06','co17','cr03','dy02','ma17']
 deleted = []
