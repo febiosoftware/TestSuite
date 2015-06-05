de6a617e0e1e554fb64439934d9ca892eeb52c11
rawlins <user@email>
2015-06-05 20:33:15 +0000

    Testing: Removed problems from mod list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@7119 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- examples.py
+++ examples.py
@@ -94,7 +94,7 @@ paramopt0 = [col[0] for col in paramopt]
 
 # These problems are new, newly modified, or deleted
 new      = []
-modified = ['cf08','congneo','incneo']
+modified = []
 deleted  = []
 # Open the nightly_std file and a temporary nightly_std file
 b_new = 0
