17da9e62f6a088965bb80d126048e17f5cfaa964
rawlins <user@email>
2016-03-03 17:59:23 +0000

    Testing: Added sh17 - sh20 to new list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@8159 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
-new = []
-modified = ['co16', 'co37', 'cr01', 'cr03', 'ho01', 'ma07', 'ma13', 'mi29', 'mi31', 'ms04', 'pi02', 'ri02', 'vc01']
+new = ['sh17','sh18','sh19','sh20']
+modified = []
 deleted = []
