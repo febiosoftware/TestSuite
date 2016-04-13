223816182822374704b6eb363c64057175e20a66
rawlins <user@email>
2016-04-13 18:14:40 +0000

    Testing: Removed mod list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@8247 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['bp05', 'bp15', 'bp16', 'bp17', 'bp19', 'bp20', 'bp22', 'bp23', 'bs04', 'bs05', 'bs06', 'bs07', 'bs08', 'co11', 'co12', 'co27', 'co43', 'dy03', 'dy04', 'ho01', 'ma16', 'ma17', 'mg02', 'mi09', 'mi29', 'mp01', 'mp02', 'mp06', 'mp07', 'mp08', 'tr01', 'tr02']
+modified = []
 deleted = []
