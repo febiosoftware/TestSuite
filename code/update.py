70dee9964954fe0a6c7f81f4a87b80ff3e173388
rawlins <user@email>
2016-12-12 23:50:36 +0000

    Testing: Removed new list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@9046 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
-new = ['sh06.feb','sh08.feb','sh09.feb','sh11.feb','sh12.feb','sh22.feb','sh23.feb','sh24.feb','sh25.feb','sh26.feb','sh27.feb','sh28.feb','sh29.feb','sh30.feb','sh31.feb','sh32.feb','sh33.feb','sh34.feb','sh35.feb','sh36.feb','sh37.feb','sh38.feb','sh39.feb','sh40.feb','sh41.feb','sh42.feb','sh43.feb','sh44.feb','sh45.feb','sh46.feb','sh47.feb','sh48.feb','sh49.feb','sh50.feb','sh51.feb','sh52.feb','sh53.feb','sh54.feb','sh55.feb','sh56.feb','sh57.feb','sh58.feb','sh59.feb']
+new = []
 modified = []
 deleted = []
