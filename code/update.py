f641e7f84986c73b8b2234223fcd814fe8fb8db5
rawlins <user@email>
2016-12-13 22:35:52 +0000

    Testing: fixed bug in mod list
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@9052 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['sh06.feb','sh08.feb','sh09.feb','sh11.feb','sh12.feb','sh22.feb','sh23.feb','sh24.feb','sh25.feb','sh26.feb','sh27.feb','sh28.feb','sh29.feb','sh30.feb','sh31.feb','sh32.feb','sh33.feb','sh34.feb','sh35.feb','sh36.feb','sh37.feb','sh38.feb','sh39.feb','sh40.feb','sh41.feb','sh42.feb','sh43.feb','sh44.feb','sh45.feb','sh46.feb','sh47.feb','sh48.feb','sh49.feb','sh50.feb','sh51.feb','sh52.feb','sh53.feb','sh54.feb','sh55.feb','sh56.feb','sh57.feb','sh58.feb','sh59.feb']
+modified = ['sh06','sh08','sh09','sh11','sh12','sh22','sh23','sh24','sh25','sh26','sh27','sh28','sh29','sh30','sh31','sh32','sh33','sh34','sh35','sh36','sh37','sh38','sh39','sh40','sh41','sh42','sh43','sh44','sh45','sh46','sh47','sh48','sh49','sh50','sh51','sh52','sh53','sh54','sh55','sh56','sh57','sh58','sh59']
 deleted = []
