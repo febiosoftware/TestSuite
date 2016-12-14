1058d25766bf76c8b701fa174a195dbd31cc1633
rawlins <user@email>
2016-12-14 23:30:28 +0000

    Testing: Added problems to mod list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@9063 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['sh06','sh08','sh09','sh11','sh12','sh22','sh23','sh24','sh25','sh26','sh27','sh28','sh29','sh30','sh31','sh32','sh33','sh34','sh35','sh36','sh37','sh38','sh39','sh40','sh41','sh42','sh43','sh44','sh45','sh46','sh47','sh48','sh49','sh50','sh51','sh52','sh53','sh54','sh55','sh56','sh57','sh58','sh59']
+modified = ['bp05','bp11','bp12','bp13','bp14','bp16','bp20','bp22','bs06','bs07','cf05','cf07','cr02','cr05','di02','di03','di04','dm12','dm13','dm14','dm15','fi12','fi13','mg01','rj14','sh02','sh03','sh04','sh05','sh10','sh17']
 deleted = []
