a2594a298aa73c17850eb0154a7792aa3d4424e0
rawlins <user@email>
2017-01-06 20:49:34 +0000

    Testing: Added problems to mod list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@9117 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,4 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = ['bp05','bp11','bp12','bp13','bp14','bp16','bp20','bp22','bs06','bs07','cf05','cf07','cr02','cr05','di02','di03','di04','dm12','dm13','dm14','dm15','fi12','fi13','mg01','rj14','sh02','sh03','sh04','sh05','sh10','sh17']
+modified = ['cr01','cr03','ho01','sh05','sh06','sh30','sh31','sh43','sh44','sh52','sh53','sh54','sh55']
 deleted = []
