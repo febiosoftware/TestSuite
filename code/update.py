d9a2320f7d5e1194756b6581ccdf5e7fbe24c90b
rawlins <user@email>
2017-02-22 23:38:50 +0000

    Testing: Added problems to mod list.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@9263 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,9 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = []
+modified = ['bp05','bp10','bp11','bp12','bp13','bp14','bp15','bp16','bp17','bp19','bp21','bp22','bp23','bs05','bs06','bs07','bs08',
+			'co01','co02','co04','co07','co09','co11','co12','co13','co15','co16','co17','co18','co19','co20','co22','co26','co28',
+			'co29','co30','co31','co32','co34','co35','co38','co39','co40','co41','co42','co43','cr02','dy04','dy09','ht01','ma06',
+			'ma11','ma11','ma14','ma15','ma16','ma18','ma19','mg01','mg02','mi16','mi26','mi32','mp06','mp07','mp08','pi01','sh05',
+			'sh06','sh16','sh17','sh18','sh22','sh23','sh24','sh26','sh28','sh29','sh30','sh31','sh54','sh55','sh56','sh57','te02',
+			'te04','te05','vc01','vc02']
 deleted = []
