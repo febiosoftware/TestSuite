9466cee1d5cc137025a2a716508fc34d61ae5d77
rawlins <user@email>
2017-09-29 22:45:17 +0000

    Testing: Added logdata updates to mod list
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@9881 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,4 +1,16 @@
 # Enter problems as: ['tr01', 'tr02']
 new = []
-modified = []
+modified = ['bp07','bp08','bp09','bs02',
+			'bs03','co43','co44','co45','co46','co47','co48','co49','cf06','cr04',
+			'dm01','dm02','dm03','dm04','dm05','dm06','dm07','dm08','dm09','dm10',
+			'dm11','dm12','dm13','dm14','dm15','dm16','ho01','ma18','ma19','ma20',
+			'ma21','ma22','ma23','ma24','ma25','ma26','ma27','ma28','ma29','ma30','ma31',
+			'mp09',
+			'rj01','rj02','rj03','rj04','rj05','rj06','rj07','rj08','rj09','rj10',
+			'rj11','rj12','rj13','rj14','rj15','sh06','sh08','sh09',
+			'sh12','sh13','sh14','sh15','sh16','sh17','sh18','sh19','sh20',
+			'sh21','sh22','sh23','sh24','sh25','sh26','sh27','sh28','sh29','sh30',
+			'sh31','sh32','sh33','sh34','sh35','sh36','sh37','sh38','sh39','sh40',
+			'sh41','sh42','sh43','sh44','sh45','sh46','sh47','sh48','sh49','sh50',
+			'sh51','sh52','sh53','sh54','sh55','sh56','sh57','sh58','sh59']
 deleted = []
