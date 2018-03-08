1b83d2a1e7f8f7d218cb18b1f68f7429e8b1138d
rawlins <user@email>
2018-03-08 21:22:01 +0000

    Testing: Added fluid problems.
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@10512 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/update.py
+++ code/update.py
@@ -1,7 +1,7 @@
 # Enter problems as: ['tr01', 'tr02']
-new = []
-modified = ['fl01','fl02','fl03','fl04','fl05','fl06','fl07','fl08','fl09','fl10',
-			'fl11','fl12','fl13','fl14','fl15','fl16','fl17','fl18','fl19','fl20',
-			'fl21','fl22','fl23','fl24','fl25','fl26','fl27','fl28','fl29','fl30',
-			'fl31','fl32','fl33','fl34','fl35','fl36','fl37']
+new = ['fl01','fl02','fl03','fl04','fl05','fl06','fl07','fl08','fl09','fl10',
+		'fl11','fl12','fl13','fl14','fl15','fl16','fl17','fl18','fl19','fl20',
+		'fl21','fl22','fl23','fl24','fl25','fl26','fl27','fl28','fl29','fl30',
+		'fl31','fl32','fl33','fl34','fl35','fl36','fl37']
+modified = []
 deleted = []
