cab7d0014d46bd25e72147ad8483c0327d358fb6
rawlins <user@email>
2018-09-13 19:48:24 +0000

    Testing: fixed bug
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Testing@11283 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- code/nightly.py
+++ code/nightly.py
@@ -31,7 +31,7 @@ print("sysplat = " + sysplat)
 # r: use the Release version of FEBio2
 
 #Run without plugins
-nopi == 1
+nopi = 1
 
 if len(sys.argv) == 1: sys.exit("error: The name of the FEBio directory must be entered as an argument")
 febio_name = sys.argv[1]
@@ -212,7 +212,7 @@ else: #Linux and OSX
 			shutil.copy(febio, febio.split('.')[0] + '_' + str(version) + '.' + platd)
 			
 			# Plugins
-			if nopi = 0
+			if nopi == 0
 				command =['make', plats]
 				output = subprocess.call(command)
 				if output != 0: print("FEBio (sequential) did not compile after updating svnrev.h")
@@ -233,7 +233,7 @@ nerrs = 0                       # nr of error terminations
 exempt = ['oi01', 'oi02', 'hi01']
 	
 # Don't run plugin problems
-if nopi = 1
+if nopi == 1
 	exempt += ['pi01', 'pi02', 'pi03', 'pi04', 'pi05', 'pi06', 'pi07', 'pi08', 'pi09']
 	
 # These problems give inconsistent convergence statistics results
